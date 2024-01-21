a08d4c88c330bccb4287ae067cc760d96de691e2
Anurag Jaiswal <janurag582004@gmail.com>
2024-01-21 17:37:35 +0530

    .

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
 
+
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
+
 
     if(distance < 0) {
         clearInterval(x);
